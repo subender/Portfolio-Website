d895df74f42193e738a37bfe9342bac7d9895b12
subender <subender.io@outlook.com>
2022-03-20 03:00:25 +0530

    implementd typed.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Typed Js
+var typed = new Typed("#type", {
+  strings: ["Subender Nath", "a Software Engineer", "into Web Development.."],
+  typeSpeed: 80,
+  startDelay: 0,
+  backSpeed: 40,
+  loop: true,
+});
